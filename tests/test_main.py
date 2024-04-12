3c03f5453a94b7f2336801ff62d8447d1d66100b
LivMad <livia.o.dantas@gmail.com>
2024-04-12 20:38:41 -0300

    Agora foi

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
-from app.main import fatorial
-from app.main import bhaskara
-from app.main import serie_taylor
+import math
+
+from app.main import bhaskara, fatorial, serie_taylor
+
 
 def test_fat_0():
     assert fatorial(0) == 1
@@ -15,14 +16,19 @@ def test_fat_5():
 
 
 def test_bhaskara_raiz_1():
-    assert bhaskara (1,-2,1) == 1
+    assert bhaskara(1, -2, 1) == 1
+
 
 def test_bhaskara_raiz_2():
-    assert bhaskara(1,-5,6) = 3, 2 
-    
-def test_bhaskara_complexo():
-    assert bhaskara(2,3,4) == "Eita, número complexo à vista!"
-    
+    resposta = bhaskara(1, -5, 6)
+    assert resposta[0] == 3.0
+    assert resposta[1] == 2.0
 
-            
-    
+
+def test_bhaskara_complexo():
+    assert bhaskara(2, 3, 4) is None
+
+
+def test_taylor():
+    valor_real = math.sin(math.pi / 4)
+    assert str(serie_taylor(math.pi / 4, 10, 1e-4))[:6] == str(valor_real)[:6]
